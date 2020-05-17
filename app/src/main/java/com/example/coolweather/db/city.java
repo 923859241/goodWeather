dd6e762186a7047f898e00b62ab8feea54ba9cd7
chen <923859241@qq.com>
2020-05-17 19:43:18 +0800

    加入数据库和表

---
+++ app/src/main/java/com/example/coolweather/db/city.java
@@ -0,0 +1,30 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class city extends LitePalSupport {
+    private String cityID;
+    private String cityName;
+    private String cityPath;
+
+    public String getCityID(){
+        return this.cityID;
+    }
+    public void setCityID(String cityID) {
+        this.cityID = cityID;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityPath() {
+        return cityPath;
+    }
+    public void setCityPath(String cityPath) {
+        this.cityPath = cityPath;
+    }
+}
